1ed058d98d9df7c8a3a54ae1f00f698e9858e3ab
paulohenriqueav <paulohenriqueav@gmail.com>
2016-10-24 16:59:43 -0200

    Script
    Script responsible for discovering the installed printers and print output by Json

---
+++ printer-discovery.sh
@@ -0,0 +1,22 @@
+#Created by: Paulo Henrique de Andrade Vaz
+#Creation Date: 24/10/2016
+#Update Date: 24/10/2016
+#Version: 1.0
+#########################################
+
+i=1
+linha=$(lpstat -a | awk '{print $1}' | wc -l)
+i=1
+printf '{"data":['
+while test $i -le $linha
+do
+        printer=$(lpstat -a | awk '{print $1}' | sed -n $i' p;')
+        i="$((i+1))"
+        printf '{"{#PRINTNAME}":"'
+        printf $printer'"}'
+        if test $i -le "$((linha))"
+        then
+        printf ','
+        fi
+done
+printf ']}'
